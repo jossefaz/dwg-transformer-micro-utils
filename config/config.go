281c267120abd93c975186dd0432ebc70b2aa6c4
yossefazoulay <azoulayos@protonmail.com>
2020-02-25 14:20:47 +0200

    remove hard coded config interface

--- config/config.go
+++ config/config.go
@@ -5,23 +5,10 @@ import (
 	"github.com/yossefazoulay/go_utils/utils"
 )
 
-type Configuration struct {
-	Queue struct {
-		Rabbitmq struct {
-			ConnString string `json:"ConnString"`
-			QueueNames struct {
-				ConvertDWG string `json:"ConvertDWG"`
-			} `json:"QueueNames"`
-		} `json:"Rabbitmq"`
-	} `json:"Queue"`
-}
-
-var configEnv = make(map[string]string)
-
-func GetConfig(env string)  Configuration {
-	configEnv["dev"] = "./config/files/config.dev.json"
-	configEnv["prod"] = "./config/files/config.prod.json"
-	configuration := Configuration{}
+func GetConfig(env string, configuration interface{})  interface{} {
+	var configEnv = make(map[string]string)
+	configEnv["dev"] = "./config/config.dev.json"
+	configEnv["prod"] = "./config/config.prod.json"
 	err := gonfig.GetConf(configEnv[env], &configuration)
 	utils.HandleError(err, "Cannot load/read config file")
 	return configuration
