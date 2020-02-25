7bda506af6492427ebe9eb8726f8da90fe41f0ea
yossefazoulay <azoulayos@protonmail.com>
2020-02-25 15:26:54 +0200

    add pointer to getconfig parameter

--- config/config.go
+++ config/config.go
@@ -5,11 +5,11 @@ import (
 	"github.com/yossefazoulay/go_utils/utils"
 )
 
-func GetConfig(env string, configuration *interface{}) {
+func GetConfig(env string, configuration interface{}) {
 	var configEnv = make(map[string]string)
 	configEnv["dev"] = "./config/config.dev.json"
 	configEnv["prod"] = "./config/config.prod.json"
-	err := gonfig.GetConf(configEnv[env], configuration)
+	err := gonfig.GetConf(configEnv[env], &configuration)
 	utils.HandleError(err, "Cannot load/read config file")
 }
 
