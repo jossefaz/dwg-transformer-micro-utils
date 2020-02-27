e7c2951837e9752fc605a228f903de5be233bb5d
yossefazoulay <azoulayos@protonmail.com>
2020-02-27 17:04:36 +0200

    create setResult func

--- utils/struct.go
+++ utils/struct.go
@@ -5,3 +5,13 @@ type PickFile struct {
 	Result map[string]int
 	From string
 }
+
+
+func SetResult(pFile *PickFile, resultKeys []string, resultVal []int, from string, path string){
+	for i, k := range resultKeys {
+		pFile.Result[k] = resultVal[i]
+	}
+	pFile.From = from
+	pFile.Path = path
+}
+
