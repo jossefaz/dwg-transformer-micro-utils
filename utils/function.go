c84c131428ce2e7f4849937b54bbe5fb5fd34640
yossefazoulay <azoulayos@protonmail.com>
2020-03-12 13:51:27 +0200

    add two conversion util for bool to int and vice versa

--- utils/function.go
+++ utils/function.go
@@ -5,3 +5,16 @@ func HandleError(err error, msg string, logger *Logger) {
 		logger.Log.Error("%s: %s", msg, err)
 	}
 }
+func Itob(i int) bool {
+	if i == 0 {
+		return false
+	}
+	return true
+}
+
+func Btoi(b bool) int {
+	if b {
+		return 1
+	}
+	return 0
+}
